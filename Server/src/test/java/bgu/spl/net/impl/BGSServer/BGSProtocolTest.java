46628ad322a0289b079b6674333dec2c03b50af0
Rotem Bar <rotembar110@gmail.com>
2022-01-05 18:07:05 +0200

    added test to PM

--- Server/src/test/java/bgu/spl/net/impl/BGSServer/BGSProtocolTest.java
+++ Server/src/test/java/bgu/spl/net/impl/BGSServer/BGSProtocolTest.java
@@ -21,6 +21,7 @@ import bgu.spl.net.impl.BGSServer.Messages.FollowMessage;
 import bgu.spl.net.impl.BGSServer.Messages.LoginMessage;
 import bgu.spl.net.impl.BGSServer.Messages.LogoutMessage;
 import bgu.spl.net.impl.BGSServer.Messages.NotificationMessage;
+import bgu.spl.net.impl.BGSServer.Messages.PMMessage;
 import bgu.spl.net.impl.BGSServer.Messages.PostMessage;
 import bgu.spl.net.impl.BGSServer.Messages.RegisterMessage;
 import bgu.spl.net.impl.BGSServer.Messages.BGSMessage.Opcode;
@@ -90,21 +91,9 @@ public class BGSProtocolTest {
 
     }
 
-
-    // private class BlockingConnectionHandlerTest extends BlockingConnectionHandler<BGSMessage> {
-
-    //     public BlockingConnectionHandlerTest(Connections<BGSMessage> connections, BidiMessagingProtocol<BGSMessage> protocol) {
-    //         super(null, connections, null, protocol);
-    //     }
-
-    // }
-
+    ////////////////// Test //////////////////
     Map<String, BGSStudent> usernamesToStudentMap = null;
     ConnectionsTest connections = null;
-
-    public BGSProtocolTest() {
-
-    }
 
     @Before
     public void setUp() {
@@ -183,6 +172,7 @@ public class BGSProtocolTest {
     @Test
     public void testLogout() {
         BGSProtocol protocol1 = this.createProtocol();
+        BGSProtocol protocol2 = this.createProtocol();
         
         LogoutMessage logoutMsg = new LogoutMessage();
 
@@ -210,8 +200,8 @@ public class BGSProtocolTest {
         protocol1.process(logoutMsg);
         
         // logout should fail.
-        connections.expectSend(protocol1.getConnectionId(), new ErrorMessage(Opcode.LOGOUT));
-        protocol1.process(logoutMsg);
+        connections.expectSend(protocol2.getConnectionId(), new ErrorMessage(Opcode.LOGOUT));
+        protocol2.process(logoutMsg);
         
         assertTrue("message left in connection", this.connections.checkAllClear());
     }
@@ -308,16 +298,21 @@ public class BGSProtocolTest {
         connections.expectSend(protocol3.getConnectionId(), new AckMessage(Opcode.LOGIN, ""));
         protocol3.process(msg6);
 
-        //////// Rotem and Shay foolow Mor.
+        //////// Mor posts to nobody
+        PostMessage post0 = new PostMessage("Post 0!!");
+        connections.expectSend(protocol1.getConnectionId(), new AckMessage(Opcode.POST, ""));
+        protocol1.process(post0);
+
+        //////// Rotem and Shay folow Mor.
         // Rotem's follow shold success
         FollowMessage msg7= new FollowMessage((byte) 0, "Mor");        
         connections.expectSend(protocol2.getConnectionId(), new AckMessage(Opcode.FOLLOW, "Mor" + '\0'));
         protocol2.process(msg7);
         
-        // Mor's follow shold success
+        // Shay's follow shold success
         FollowMessage msg8= new FollowMessage((byte) 0, "Mor");        
         connections.expectSend(protocol3.getConnectionId(), new AckMessage(Opcode.FOLLOW, "Mor" + '\0'));
-        protocol3.process(msg7);
+        protocol3.process(msg8);
 
         ///////// Shay logout
         // Shay logout should success
@@ -345,6 +340,107 @@ public class BGSProtocolTest {
         connections.expectSend(protocol4.getConnectionId(), new NotificationMessage((byte)1, "Rotem", "Post 1 @Shay"));
         protocol2.process(msg12);
 
+        assertTrue("message left in connection", this.connections.checkAllClear());
+    }
+
+
+
+    @Test
+    public void testPM() {
+        BGSProtocol protocol1 = this.createProtocol();
+        BGSProtocol protocol2 = this.createProtocol();
+        BGSProtocol protocol3 = this.createProtocol();
+        BGSProtocol protocol4 = this.createProtocol();
+
+        // Mor register should success
+        RegisterMessage msg1 = new RegisterMessage("Mor", "123 ", "29-12-1997");
+        connections.expectSend(protocol1.getConnectionId(), new AckMessage(Opcode.REGISTER, ""));
+        protocol1.process(msg1);
+        assertEquals(1, this.usernamesToStudentMap.size());
+
+        ////////// Register
+        // Rotem register should success
+        RegisterMessage msg2 = new RegisterMessage("Rotem", "123 ", "29-12-1997");
+        connections.expectSend(protocol2.getConnectionId(), new AckMessage(Opcode.REGISTER, ""));
+        protocol2.process(msg2);
+        assertEquals(2, this.usernamesToStudentMap.size());
+
+        // Shay register should success
+        RegisterMessage msg3 = new RegisterMessage("Shay", "123 ", "29-12-1997");
+        connections.expectSend(protocol2.getConnectionId(), new AckMessage(Opcode.REGISTER, ""));
+        protocol2.process(msg3);
+        assertEquals(3, this.usernamesToStudentMap.size());
+        
+        ////////// Login
+        // Mor login should success
+        LoginMessage msg4 = new LoginMessage("Mor", "123 ", (byte)1);
+        connections.expectSend(protocol1.getConnectionId(), new AckMessage(Opcode.LOGIN, ""));
+        protocol1.process(msg4);
+
+        // Rotem login should success
+        LoginMessage msg5 = new LoginMessage("Rotem", "123 ", (byte)1);
+        connections.expectSend(protocol2.getConnectionId(), new AckMessage(Opcode.LOGIN, ""));
+        protocol2.process(msg5);
+
+        //////// Basic 
+        // Shay PM while not connected should fail.
+        PMMessage pm0 = new PMMessage("Rotem", "PM 0!!", "12-12-1212");
+        connections.expectSend(protocol3.getConnectionId(), new ErrorMessage(Opcode.PM));
+        protocol3.process(pm0);
+        
+        // Rotem PM to non-existing user should fail
+        PMMessage pm1 = new PMMessage("blabla", "PM 1!!", "12-12-1212");
+        connections.expectSend(protocol2.getConnectionId(), new ErrorMessage(Opcode.PM));
+        protocol2.process(pm1);
+        
+        // Rotem PM not following mor - should fail
+        PMMessage pm2 = new PMMessage("Mor", "PM 2!!", "12-12-1212");
+        connections.expectSend(protocol2.getConnectionId(), new ErrorMessage(Opcode.PM));
+        protocol2.process(pm2);
+
+        //////// Login Shay
+        // Shay login should success
+        LoginMessage msg6 = new LoginMessage("Shay", "123 ", (byte)1);
+        connections.expectSend(protocol3.getConnectionId(), new AckMessage(Opcode.LOGIN, ""));
+        protocol3.process(msg6);
+
+        //////// Rotem and Shay folow Mor.
+        // Rotem's follow shold success
+        FollowMessage msg7= new FollowMessage((byte) 0, "Mor");        
+        connections.expectSend(protocol2.getConnectionId(), new AckMessage(Opcode.FOLLOW, "Mor" + '\0'));
+        protocol2.process(msg7);
+        
+        // Shay's follow shold success
+        FollowMessage msg8= new FollowMessage((byte) 0, "Mor");        
+        connections.expectSend(protocol3.getConnectionId(), new AckMessage(Opcode.FOLLOW, "Mor" + '\0'));
+        protocol3.process(msg8);
+
+        //////// Rotem PMs to mor
+        // PM should success.
+        PMMessage pm3 = new PMMessage("Mor", "PM 3!!", "12-12-1212");
+        connections.expectSend(protocol2.getConnectionId(), new AckMessage(Opcode.PM, ""));
+        connections.expectSend(protocol1.getConnectionId(), new NotificationMessage((byte)0, "Rotem", "PM 3!! 12-12-1212"));
+        protocol2.process(pm3);
+        
+        ///////// Mor logout
+        // logout should success
+        LogoutMessage msg9 = new LogoutMessage();
+        connections.expectSend(protocol1.getConnectionId(), new AckMessage(Opcode.LOGOUT, ""));
+        protocol1.process(msg9);
+        
+
+        //////// Rotem PMs to mor again, now with filter
+        // PM should success.
+        PMMessage pm4 = new PMMessage("Mor", "PM 4!! Kaki", "12-12-1212");
+        connections.expectSend(protocol2.getConnectionId(), new AckMessage(Opcode.PM, ""));
+        protocol2.process(pm4);
+
+        //////// Mor login again and get 
+        LoginMessage msg10 = new LoginMessage("Mor", "123 ", (byte)1);
+        connections.expectSend(protocol4.getConnectionId(), new AckMessage(Opcode.LOGIN, ""));
+        connections.expectSend(protocol4.getConnectionId(), new NotificationMessage((byte)0, "Rotem", "PM 4!! <filtered> 12-12-1212"));
+        protocol4.process(msg10);
+        
         assertTrue("message left in connection", this.connections.checkAllClear());
     }
 }
